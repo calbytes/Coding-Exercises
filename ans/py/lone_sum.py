07fad4c02a39f6db56ede9d38dfe2b52ca01e5fc
calbytes <caalla3@gmail.com>
2023-11-23 08:48:10 -0500

    lone_sum()

--- ans/py/lone_sum.py
+++ ans/py/lone_sum.py
@@ -1,12 +1,23 @@
+#CodingBat, Logic 2 -> lone_sum
+
 def lone_sum(a, b, c):
-  dict = {a: 1}
-  if b in dict:
-    dict[b] = -1
-  if c in dict:
-    dict[c] = -1
-    
-  res = 0
-  for i in dict:
-    if dict[i] == 1:
-      res += i
-  return res
+    dict = {a: 1}
+
+    if b in dict:
+        dict[b] = -1
+    else:
+        dict[b] = 1
+
+    if c in dict:
+        dict[c] = -1
+    else:
+        dict[c] = 1
+
+    res = 0
+    for i in dict:
+        if dict[i] == 1:
+            res += i
+    return res
+
+
+print(lone_sum(1,2,3))
